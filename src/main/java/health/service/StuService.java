c4c7b5ee036f0b6ad48127ede6086d0d06137cd8
fei li <www.1259846094@qq.com>
2020-10-13 21:42:30 +0800

    完成异常信息界面的显示以及分类查询数据(并且可以新建伪数据，在删除的controller方法中)

--- src/main/java/health/service/StuService.java
+++ src/main/java/health/service/StuService.java
@@ -9,5 +9,11 @@ import java.util.List;
 public interface StuService {
     int insert(student record);
     PageInfo<student> getStuList(int page,int limit);
-       void delByDateAndTelephone(delList data);
+    PageInfo<student> getStuList_fever(int page,int limit);
+    PageInfo<student> getStuList_contact(int page,int limit);
+    PageInfo<student> getStuList_situation(int page,int limit);
+    void delByDateAndTelephone(delList data);
+    int insertPseudoData(delList data);
+    PageInfo<student> getStuListByClassId(int page,int limit,student student);
+    PageInfo<student> getStuListByStudentName(int page,int limit,student student);
 }
--- src/main/java/health/service/StuServiceImpl.java
+++ src/main/java/health/service/StuServiceImpl.java
@@ -34,7 +34,52 @@ public class StuServiceImpl implements StuService {
     }
 
     @Override
+    public PageInfo<student> getStuList_fever(int page, int limit) {
+        PageHelper.startPage(page, limit);
+        List<student> allStudents = studentMapper.getStudents_fever();
+        PageInfo<student> pageInfo=new PageInfo<student>(allStudents);
+        return pageInfo;
+    }
+
+    @Override
+    public PageInfo<student> getStuList_contact(int page, int limit) {
+        PageHelper.startPage(page, limit);
+        List<student> allStudents = studentMapper.getStudents_contact();
+        PageInfo<student> pageInfo=new PageInfo<student>(allStudents);
+        return pageInfo;
+    }
+
+    @Override
+    public PageInfo<student> getStuList_situation(int page, int limit) {
+        PageHelper.startPage(page, limit);
+        List<student> allStudents = studentMapper.getStudents_situation();
+        PageInfo<student> pageInfo=new PageInfo<student>(allStudents);
+        return pageInfo;
+    }
+
+    @Override
     public void delByDateAndTelephone(delList data) {
         studentMapper.delByDateAndTelephone(data);
+    }
+
+    @Override
+    public int insertPseudoData(delList data) {
+        return studentMapper.insertPseudoData(data);
+    }
+
+    @Override
+    public PageInfo<student> getStuListByClassId(int page, int limit,student student) {
+        PageHelper.startPage(page, limit);
+        List<student> allStudents = studentMapper.getStudentsByClassId(student);
+        PageInfo<student> pageInfo=new PageInfo<student>(allStudents);
+        return pageInfo;
+    }
+
+    @Override
+    public PageInfo<student> getStuListByStudentName(int page, int limit, student student) {
+        PageHelper.startPage(page, limit);
+        List<student> allStudents = studentMapper.getStudentsByStudentName(student);
+        PageInfo<student> pageInfo=new PageInfo<student>(allStudents);
+        return pageInfo;
     }
 }
